33711ae355a4c82f3cc5b0e8c2b478ce44ec94cb
Yasir <yasirbashir038@gmail.com>
2020-10-04 11:39:26 +0500

    new class added

---
+++ src/opennlp/NewClass.java
@@ -0,0 +1,18 @@
+package opennlp;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Yacir bashir
+ */
+public class NewClass {
+    public void show()
+    {
+        System.out.println("hello wolrd");
+    }
+}
